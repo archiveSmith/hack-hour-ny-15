d58046e7113e2a08b515597a31e0f66070a4fe8b
jenae <jenaepen@gmail.com>
2019-11-05 09:58:16 -0500

    finish challenge

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -17,12 +17,42 @@
  */
 
 function Node(val) {
-  this.value = val;
-  this.next = null;
+    this.value = val;
+    this.next = null;
 }
 
 function kthToLastNode(k, head) {
+    if (k <= 0) k = 1;
+    let length = 0;
 
+    let node = head;
+    while (node) {
+        length++;
+        node = node.next;
+    }
+    node = head;
+    let position = 0;
+    while (position < length - k) {
+        position++;
+        node = node.next;
+    }
+    return node.value;
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+//kthToLastNode(2, a); // -> returns 'D' (the value on the second to last node)
+console.log(' kthToLastNode(0, a):', kthToLastNode(7, a))
+
+
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };
