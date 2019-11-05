3524a1bf2ff408c550d60da21a2727a2519f938d
James Gary <gmarchesi888@gmail.com>
2019-11-05 09:47:46 -0500

    Solved kth-to-last-node.

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,26 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  const allVals = [];
+  let currentNode = head;
+  while (currentNode.next !== null) {
+    allVals.push(currentNode.value);
+    currentNode = currentNode.next;
+  }
+  return allVals[allVals.length - 1];
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+const e = new Node("E");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(2, a)); // returns 'D' (the value on the second to last node)
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };
