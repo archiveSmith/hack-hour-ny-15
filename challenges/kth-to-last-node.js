02c040f032b6c7263669b1ce56ca9f53344ce834
Chris Romano <chrispaulromano@gmail.com>
2019-11-05 10:19:48 -0500

    HH

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,46 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
+ const a = new Node('A');
+ const b = new Node('B');
+ const c = new Node('C');
+ const d = new Node('D');
+ const e = new Node('E');
 
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
+
+
+function kthToLastNode(k, head) {
+  let counter = 1;
+  function llCounter (head) {
+    if (!head.next) {
+      return counter
+    }
+    else {
+    counter += 1
+    }
+    head = head.next
+    return llCounter(head)
+  }
+  llCounter(head)
+  function retrieve (head) {
+    let innerCounter = 6
+    while (innerCounter != counter + 1 - k){
+      function anotherFunc (head) {
+        innerCounter += 1
+        head = head.next
+        return head.next
+      }
+      anotherFunc(head)
+    }
+    return head.next.value
+  }
+  let result = retrieve(head)
+  return result
 }
+console.log(kthToLastNode(5, a))
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
