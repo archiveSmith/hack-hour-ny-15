9b237daf2e40f2bf015973b434fcb7bb89be62d1
James <me@jameshighsmith.com>
2019-11-05 10:16:47 -0500

    adding first three challenges

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,33 @@
 */
 
 function drawStairs(n) {
+    // we want to construct the first string of n -1 of space
+    // from index 0 to index n - 2. 
+    // and 1 stair at position n - 1.
+    let count = n;
+    // console.log(count);
+    
+    for (let i = 0; i < n; i++) {
+        // first is 5 spaces and 1 stair
+        let spaceCount = n - 1 - i;
+        let stairCount = i + 1; 
+        // console.log(spaceCount, stairCount);
+        // space
+        let space = ' ';
+        // stairs
+        let stair = '*';
+        // 
+        // remove first element space
+        // add last element stair
+        
+        let currentRow = space.repeat(spaceCount) + stair.repeat(stairCount);
+        // let output = currentRow.join('')
+        console.log(currentRow);
+        
 
+    }
 }
 
+drawStairs(50);
 
 module.exports = drawStairs;
