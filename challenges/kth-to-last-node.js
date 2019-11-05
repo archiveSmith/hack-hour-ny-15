dc4aa7c73e98b71320f3df40ff282784ddc53b78
german Rovati <germanrovati@Germans-MacBook-Pro-3.local>
2019-11-05 10:07:42 -0500

    complete

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,37 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let curr = head;
+  let listLength = 0;
 
+  while (curr) {
+    listLength += 1;
+    curr = curr.next;
+  }
+
+  const position = listLength - (k - 1);
+  let newCount = 0;
+  let newCurr = head;
+
+  while (newCurr) {
+    newCount += 1;
+    if (position === newCount) {
+      return newCurr.value;
+    }
+    newCurr = newCurr.next;
+  }
 }
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
+console.log(kthToLastNode(2, a));
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };
