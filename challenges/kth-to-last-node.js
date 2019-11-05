335d55501be8151ea55e9e68c5081a0114a18d4e
Joseph Michael Corrado <joeyycorrss@gmail.com>
2019-11-05 09:59:21 -0500

    Linked Lists are my sitch

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,51 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  // CHECK IF INPUT IS BELOW RANGE
+  if (k < 1) 
+    return 'ERROR: OUT OF RANGE - INPUT (k) TOO SMALL';
 
+  // DECLARE VARIABLES TO COUNT LENGTH OF ARRAY 
+  // AND POINT AT CURRENT NODE
+  let count = 0;
+  let pointer = head;
+
+  // RUN THROUGH LIST AND INCREMENT COUNT TO FIND LENGTH
+  while (pointer !== null) {
+    count++;
+    pointer = pointer.next;
+  }
+
+  // CHECK IF INPUT IS ABOVE, OR PAST, RANGE
+  if (k > count)
+    return 'ERROR: OUT OF RANGE - INPUT (k) TOO LARGE';
+
+  // REASSIGN POINTER BACK TO STARTING HEAD NODE
+  pointer = head;
+
+  // DECLARE VARIABLE TO TRACK CURRENT POSITION IN LIST
+  // THEN ITERATE THROUGH LIST TO CORRECT POSITION
+  let position = 1;
+  while (position <= count - k) {
+    pointer = pointer.next;
+    position++;
+  }
+  
+  // RETURN VALUE OF POINTER WHICH SHOULD POINT AT PROPER NODE
+  return pointer.value;
 }
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(8, a)); //-> returns 'D'
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
