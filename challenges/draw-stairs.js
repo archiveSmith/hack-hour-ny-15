4aa5d79e01e2dd74d45aa2a8cdc40a73af384716
Arlo Guiwo <arloguiwo@gmail.com>
2019-11-05 09:23:08 -0500

    reupdating from old challenges

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
@@ -14,8 +14,20 @@
 */
 
 function drawStairs(n) {
-
+    if (n >= 1 && n <= 100) {
+        let stairString = '';
+        const stair = '*';
+        const space = ' ';
+        for (let row = 0; row < n; row++) {
+            stairString += space.repeat(n - (row + 1));
+            stairString += stair.repeat(row + 1);
+            stairString += '\n';
+        }
+        return console.log(stairString);
+    }
+    return console.log('not a number!');
 }
 
+drawStairs(10);
 
 module.exports = drawStairs;
--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,56 @@
  *
  */
 
-
 function modemean(array) {
+    // get mode
+    // iterate through array tracking how many times you've seen an element
+    // keep track of which element has occured the most as a variable 'occuredMost'
+    // if two elements have the same highest value, compare which one is greater and assign that value to occuredMost
 
+    // get mean
+    // get sum of all elements in the array and assign it to a total variable
+    // divide the total by the length
+    // assign that value to 'mean'
+
+    let mostFrequentValue = 0;
+    let highestFrequency = 0;
+
+    const dataAverages = array.reduce(
+        (averages, current) => {
+            // get mode
+            if (!averages.mode[current]) averages.mode[current] = 1;
+            averages.mode[current] += 1;
+            if (averages.mode[current] > highestFrequency) {
+                highestFrequency = averages.mode[current];
+                mostFrequentValue = current;
+            }
+            if (
+                averages.mode[current] === highestFrequency
+                && current > mostFrequentValue
+            ) {
+                mostFrequentValue = current;
+            }
+            // get total
+
+            averages.mean.total += current;
+            return averages;
+        },
+        {
+            mode: {},
+            mean: {
+                total: 0,
+            },
+        },
+    );
+    const mode = mostFrequentValue;
+    const mean = Math.floor(dataAverages.mean.total / array.length);
+
+    return mean === mode;
 }
+
+const shouldBeTrue = [5, 5, 5, 5, 5];
+const shouldBeFalse = [6, 6, 6, 6, 6, 5];
+console.log(modemean(shouldBeTrue));
+console.log(modemean(shouldBeFalse));
 
 module.exports = modemean;
