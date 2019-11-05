ca6a995f4d6fa94184f6db3811dbd6610ae2ba51
Sejan Miah <sejan.miah@gmail.com>
2019-11-05 09:56:58 -0500

    Track kth to last element in singly linked list

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -20,9 +20,41 @@ function Node(val) {
   this.value = val;
   this.next = null;
 }
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
 
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(a.next);
 function kthToLastNode(k, head) {
+  let tail;
+  while (head !== null) {
+    head = head.next
+    // tail = head.next
 
+    if (head.next) {
+      tail = head.next
+    }
+    console.log("tail", tail)
+
+    //get to last node and then find a way to count backwards to get to the value that i want
+  
+  }
+  return tail
+  //first find last node 
 }
+
+kthToLastNode(1, a);
+
+
+
+
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
