5de8ebdfe3fd9c0be49bba98b420598c645b7628
David Yedid <davidyedid@Davids-MacBook.local>
2019-11-06 09:01:01 -0500

    Completed kthToLastNode

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,71 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  if (k < 1) return 'enter node greater than zero'
+  let curr = head;
+  let counter = 0;
+  while (curr) {
+    counter += 1;
+    curr = curr.next
+  }
+  if (counter < k) return 'node does not exist'
+  let targetLinkNumber = counter - k;
+  curr = head;
+  while (targetLinkNumber > 0) {
+    targetLinkNumber -= 1
+    curr = curr.next
+  }
+  return curr.value 
 }
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+console.log(kthToLastNode(2, a))
+
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+
+// function Node(val) {
+//   this.value = val;
+//   this.next = null;
+// }
+
+// function kthToLastNode(k, head) {
+//   const nextSuffix = ".next"
+//   let searchStr = nextSuffix.repeat(k);
+//   console.log(searchStr)
+//   // searchStr = eval(searchStr.slice(1))
+//   let curr = head;
+//   console.log(curr + )
+
+
+//   if (curr + searchStr) {
+//     console.log('does not exist')
+//     return 'does not exist'; 
+//   }
+//   while (true) {
+//     if (curr.next.next === null) {
+//       console.log('this works')
+//       return curr.value
+//     }
+//     console.log(curr)
+//     console.log(`${curr}`)
+//     if (`${curr}${searchStr}` === null) {
+//       return curr.value;
+//     }
+//     console.log(curr)
+//     if (curr.next === null) {
+//       return 'error- no.next'
+//     }
+//       curr = curr.next
+//   }
+//   console.log('error')
+//   return 'error Return'
+// }
