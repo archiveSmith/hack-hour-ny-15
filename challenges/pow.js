c6f27232f7fe149f4dca95eab13a4fe783e654d6
Ola Adedoyin <olalinux03@gmail.com>
2019-11-06 09:51:29 -0500

    completed pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,26 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+// function pow(base, power) {
+//     if(power === 0) return 1;
+//     if(power === 1) return base;
+//     let result = base;
+//     while(power > 1){
+//         result *= base;
+//         power -= 1
+//     }
 
+//     return result;
+// }
+
+//using recursion
+function pow(base, power){
+    if(power === 0) return 1;
+    if(power === 1) return base;
+    let result = base;
+    return result * pow(base, power-=1);
 }
 
+
+//console.log(pow(2, 5))
 module.exports = pow;
