a252a9c16a78d2badc8968272c34771fefba67f7
Georgina Carr <carre.georgina@gmail.com>
2019-11-06 09:50:59 -0500

    initial pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,24 @@
  * Use recursion!
  */
 
+ //calculate the power of a raised base (ex: 2^4 is 2 * 2 * 2 * 2 => 16) 
+ //create base case to exit function
+//if power is less than or equal to 1 return base 
+//else call function and multiply the base by itself based on the number of times 
+//the power is being raised to and  
+//decrement the power on each iteration of the call of the function to get closer to the base case of 1.
+
 function pow(base, power) {
 
+    if(power <= 1) {
+        return base; 
+    }
+    
+    return base * pow(base, power - 1); //calculate the power raised to 
 }
+
+console.log(pow(3, 4));
+console.log(pow(2, 4));
+console.log(2, 1)
 
 module.exports = pow;
