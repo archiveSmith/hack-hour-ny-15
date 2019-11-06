f53c74006d4d478c68be6d11366c8d4886c867c5
jenae <jenaepen@gmail.com>
2019-11-06 09:46:56 -0500

    handles negative nums too

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,17 @@
  */
 
 function pow(base, power) {
-
+    let sign = "+";
+    if (power === 0) return 1;
+    if (power < 0) {
+        power = Math.abs(power)
+        sign = "-";
+    }
+    if (sign === "-") return 1 / (base * pow(base, power - 1));
+    else return base * pow(base, power - 1);
 }
 
+console.log('pow(3, -1):', pow(3, -1));
+console.log('pow(3, 2):', pow(3, 2));
+console.log('pow(3, 0):', pow(3, 0));
 module.exports = pow;
