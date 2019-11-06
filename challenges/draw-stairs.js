69c27f90600daceae73d78455edd64f91e13b579
Tim <tim@Tims-MacBook-Pro.local>
2019-11-05 22:10:33 -0500

    old code transferred

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,7 +14,27 @@
 */
 
 function drawStairs(n) {
+    //declare a final stair variable
+    let finalStair;
+    //we declare an empty space variable
+    let emptySpace = ' ';
+    //keep track of how many spaces we got
+    let emptySpaceCount = n - 1;
+    //we declare a '*' variable and while on the first loop
+    let stair = '*';
+    //keep track of how many stairs we got right now
+    let stairCount = 1;
+    //we count down the passed in n and append more spaces until spaces match the string
+    // for (let i = 1; i <= n; i += 1) {
+    //     console.log('\n' + emptySpace.repeat(emptySpaceCount) + stair.repeat(stairCount));
+    //     emptySpaceCount -= 1;
+    //     stairCount += 1;
+    // }
 
+    for (let i = 1; i <= n; i++) {
+        console.log('\n' + ' '.repeat(n - i) + '*'.repeat(i))
+    }
+    
 }
 
 
