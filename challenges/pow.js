ad206da00ddbd3ab193ec9173ba652c789c6a0ae
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2019-11-06 09:54:50 -0500

    pow.js

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,19 @@
  */
 
 function pow(base, power) {
+  if (power <= 1) return base;
 
+  return base * pow(base, power - 1);
 }
+
+console.log(pow(4, 3));
+
+// tail call needs a total param
+function powTC(base, power, total = 1) {
+  if (power <= 0) return total;
+  return powTC(base, power - 1, base * total);
+}
+
+console.log(powTC(4, 3));
 
 module.exports = pow;
