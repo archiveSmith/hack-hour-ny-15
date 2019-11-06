64caafb26538f674af5ec5a485fcbccf3a72c957
Joseph Michael Corrado <joeyycorrss@gmail.com>
2019-11-06 09:43:44 -0500

    POWer-clap after three 1-2-3

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,13 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base = 1, power = -1, ogBase = base) {
+  if (power < 0) return "that's wack";
+  if (power === 0) return 1;
+  if (power === 1) return base;
+  return pow(base * ogBase, --power, ogBase);
 }
+
+// console.log(pow());
 
 module.exports = pow;
