a536b6c59e2ddbde78ae99bcd9b19011b0c3d095
Vessy <v.shestorkina@gmail.com>
2019-11-05 23:14:47 -0500

    kth to last

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -23,6 +23,36 @@ function Node(val) {
 
 function kthToLastNode(k, head) {
 
+// how do we capture what index of that node is
+// once we have the index loop through the elements and return the one at index - k
+
+let count = 0; 
+let currentNode = head;
+
+const storeArray = [];
+function innerFunc(k, currentNode) {
+  storeArray.push(currentNode);
+  if (!currentNode.next) {
+    return storeArray[storeArray.length - k];
+  }
+  return innerFunc(k, currentNode.next)
+}
+ return innerFunc(k,head)
+  // not found 
 }
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+kthToLastNode(3, a);
+
+console.log(kthToLastNode(4, a))
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
