03fbfaceafb0e04225cd20ad9c309c63c8dccc31
James Gary <gmarchesi888@gmail.com>
2019-11-07 09:22:48 -0500

    Added pow.js

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,11 @@
  */
 
 function pow(base, power) {
-
+  if (power === 1) return base;
+  return base * pow(base, power - 1);
 }
+
+console.log(pow(2, 8));
+console.log(pow(3, 3));
 
 module.exports = pow;
