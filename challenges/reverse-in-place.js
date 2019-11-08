93e65b6391bef7ac65961c74532e94bf3b7ef7c3
Georgina Carr <carre.georgina@gmail.com>
2019-11-08 10:00:50 -0500

    initial commit

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -13,8 +13,26 @@
  * DO NOT USE THE BUILT IN REVERSE METHOD
  */
 
-function reverseInPlace(array) {
 
-}
+function reverseInPlace(array) {
+    array.length = array.length * 2;
+
+   for (let i = 0; i < array.length; i++) {
+     
+     array[array.length - i - 1] = array[0]; 
+     
+     array.shift();
+     
+     //console.log(array); //[ 'b', 5, 4, 3, <4 empty items>, 'a' ]
+   } 
+   return array;
+ }
+ 
+ let reversedArray = ['a', 'b', 5, 4, 3]
+ reverseInPlace(reversedArray);
+
+
+
+
 
 module.exports = reverseInPlace;
