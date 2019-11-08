5c72212f020f0ba9349365322a01229d928bb5b4
Rex Osariemen <rexosariemen@gmail.com>
2019-11-08 09:45:50 -0500

    Attemp Hack hour problem

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -15,6 +15,17 @@
 
 function reverseInPlace(array) {
 
+    for (let i = 0; i < array.length / 2; i += 1) {
+        let temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+
+    return array
 }
+
+const stringArray = ['a', 'b', 'c', 'd', 'e', 'f'] //-> ['f', 'e', 'd', 'c', 'b', 'a']
+console.log(reverseInPlace(stringArray))
+
 
 module.exports = reverseInPlace;
