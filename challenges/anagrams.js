26ac0d96a533668928893bd3a8be639dffef90a3
Tim <tim@Tims-MacBook-Pro.local>
2019-11-08 09:28:28 -0500

    .

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -14,6 +14,35 @@
 
 function anagrams(string) {
 
+  //declare a variable to hold the output strings
+  //for each loop of the string, 
+  //for each letter in the string, concatenate and make a new combination on each iteration
+  //do the same process in reverse
+  //
+  let result = [];
+  let anaWord = '';
+  let innerCounter = string.length * 2;
+  while (innerCounter > 0) {
+    for (let i = 0; i < string.length; i++) {
+      if (innerCounter !== i) {
+        anaWord += string[i];
+
+        result.push(anaWord);
+      }
+    }
+    innerCounter -= 1;
+  }
+
+  return result;
+
 }
 
+
+function anagram (string) {
+  let resultStr = [];
+
+  //loop through the string and pass each result a
+}
+
+console.log(anagrams('abc'));
 module.exports = anagrams;
