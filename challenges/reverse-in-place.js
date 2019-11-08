5628ff4da471a5bafc3fa27f2a01d074cc4eb8b6
Tammy Tan <tammytan95@Tammys-MacBook-Air.local>
2019-11-08 10:00:29 -0500

    solved

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /**
  * Write a function to reverse an array in place
  *
@@ -14,7 +14,37 @@
  */
 
 function reverseInPlace(array) {
+  //"abefcd" --> "dcfeba"
+  //swap 0 and 5; 1 and 4; 2 and 3, stop at i ===3
 
+  //sol 1: use swap
+  //sol 2 using object:
+  //create an extra placeholder index at end
+  //starting from charAt length-1, set it equal to placeholder index
+  //0: undefined, 1: a, 2:b, 3:c
+  //loop through and set 0 === c
+
+  //challenge: get j to decrement
+  //get swap to work
+
+  for (let i = 0; i < array.length / 2; i++) {
+    console.log("i", i);
+    //0, 5; j stops at 5
+    //i = 1, j = 6-1-1 = 4
+    //arr len = 6
+    let j = array.length - i - 1;
+    console.log("j", j);
+    swap(array, i, j);
+  }
+  return array;
+
+  function swap(array, char1, char2) {
+    let temp = array[char1];
+    array[char1] = array[char2];
+    array[char2] = temp;
+  }
 }
 
+const test = ["a", "b", "e", "f", "c", "d"];
+console.log(reverseInPlace(test));
 module.exports = reverseInPlace;
