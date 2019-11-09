7caaf50edc365b186875755b2adc27dd3f1fb159
anna <anna.svetlov@gmail.com>
2019-11-09 09:46:25 -0500

    String rotation works for hello, might be edge cases where it doesn't work?

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,24 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+  //find where the firt letter of st1 lives in st2
+  //find that letter in st2 and move down until reaching end of st2
+  //that is the beginning of the word, and the rest is added to the end
+  //run isSubstring to see if they're the same word?
+    let start = s2.indexOf(s1[0])
+    let s1Index = 0;
+    let string = '';
+    for (let i = start; i < s2.length; i++)  {
+      if (s2[i] === s1[s1Index]) {
+        string += s2[i]
+        s1Index += 1;
+      }
+    }
+    string += (s2.slice(0, start))
+    //console.log(string)
+    return isSubstring(string, s1)
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+
+//console.log(stringRotation("hello", "ollhe"));
