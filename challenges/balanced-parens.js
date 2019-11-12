9d3146d1c44e70b2dba5e15ecf2e88741b9a26ac
bahrambds <bahrambds@gmail.com>
2019-11-12 09:59:18 -0500

    missing edge case, possible sol; add array add opening check if opening is last

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,37 @@
  */
 
 function balancedParens(input){
+    let pCount = 0
+    let cCount = 0 
+    let bCount = 0
 
+    for (let char of input){
+        switch(char){
+        case '(' : pCount += 1; continue
+        case ')' : 
+            pCount -= 1
+            if (cCount > 0 || bCount >0)
+                return false
+            else continue
+        case '{' : cCount += 1; continue
+        case '}' : 
+            cCount -= 1
+            if (pCount > 0 || bCount >0)
+                return false
+            else continue
+        case '[' : bCount += 1; continue
+        case ']' : 
+            bCount -= 1 
+            if (pCount > 0 || cCount >0)
+                return false
+            else continue
+        }
 }
+return (pCount === 0 && bCount === 0 && cCount === 0)   
+}
+
+console.log(balancedParens('[](){}'))
+console.log(balancedParens('[({})]'))
+console.log(balancedParens('[(]{)}'))
 
 module.exports = balancedParens;
