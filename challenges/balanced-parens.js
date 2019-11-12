b688020fbe0cd807460f0be956ea3efdbfdd1ae0
Tammy Tan <tammytan95@Tammys-MacBook-Air.local>
2019-11-12 10:11:30 -0500

    hh complete

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,48 @@
  *
  */
 
-function balancedParens(input){
+function balancedParens(input) {
+  let stack = [];
+  let brackets = {
+    "[": "]",
+    "{": "}",
+    "(": ")"
+  };
 
+  let traverseArr = input.split("");
+  let keys = Object.keys(brackets); // [,{,(
+  let vals = Object.values(brackets); // ],},)
+
+  let lastChar = stack[stack.length - 1];
+
+  //if i is in object.keys, push into traverse ([,{,()
+  //if  stack is not empty, last char in stack === object.key[i] ],},)
+  //stack[stack.length - 1] -->  last char in stack --> {
+  //brackets[lastChar] --> }
+  //traverseArr[i] --> } --> if this is in vals, find index
+
+  for (let i = 0; i < traverseArr.length; i++) {
+    if (keys.includes(traverseArr[i])) {
+      stack.push(traverseArr[i]);
+    } else if (
+      stack.length > 0 &&
+      brackets[stack[stack.length - 1]] === traverseArr[i]
+    ) {
+      //   console.log(traverseArr[i]);
+      //   console.log(stack);
+      stack.pop();
+    }
+  }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+// [,
+
+const testStr = "[{](){"; // true
+console.log(balancedParens(testStr)); // false
 
 module.exports = balancedParens;
--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -3,20 +3,32 @@
  * true if the second string is found in the first, false otherwise.
  *
  * h->e->l->l->o   ===   l->l-o->h->e
+ *
  * h->e->l->l->o   !==   o->l-l->h->e
  *
- * For example: stringRotation("hello", "hello") -> true
- *              stringRotation("hello", "llohe") -> true
+ * For example: stringRotation("hello", "hello") -> true //hello
+ *              stringRotation("hello", "llohe") -> true //swap he with llo
  *              stringRotation("hello", "he") -> false
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
  */
 
 function isSubstring(s1, s2) {
+  //looks for index of first occurence of s2 in s1. if cannot find, return -1
+  console.log(s1.indexOf(s2));
   return s1.indexOf(s2) >= 0;
 }
 
 function stringRotation(s1, s2) {
-
+  //need to find "he" in order to return val >0
+  //point h > e > l > l> o
+  //split str into charArr
+  let s2Char = s2.split("");
+  for (let i = 0; i < s2Char.length - 1; i++) {}
 }
 
-module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+const test1 = "hello";
+const test2 = "llohe";
+const test3 = "lohel";
+console.log(isSubstring(test1, test2));
+
+module.exports = { isSubstring: isSubstring, stringRotation: stringRotation };
