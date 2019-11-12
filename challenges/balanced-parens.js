6cece2b17756856c0493cbe7957ba22776adccdc
jenae <jenaepen@gmail.com>
2019-11-12 09:59:30 -0500

    finished parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,77 @@
  */
 
 function balancedParens(input){
+    let objParens = {};
+    for(let i=0; i< input.length; i++){
+      
+        if(input[i] === "{" || input[i] === "(" || input[i] === "[" ){
+            console.log('input[i]:', input[i])
+            if(objParens[input[i]]) objParens[input[i]].push(i)
+            else objParens[input[i]] = [i]
+        }
+        if(input[i] === "}"){
+            console.log('input[i]:', input[i])
+           if(lastParens(objParens) !== "{") return false;
+           else {
+            objParens["{"].pop(); 
+            if(objParens["{"].length === 0){
+                delete objParens["{"]
+            }
+           }
+        }
+        if(input[i] === ")"){
+            console.log('input[i]:', input[i])
+            if(lastParens(objParens) !== "(") return false;
+            else {
+                objParens["("].pop(); 
+                if(objParens["("].length === 0){
+                    delete objParens["("]
+                }
+            }
+        }
+        if(input[i] === "]"){
+            console.log('input[i]:', input[i])
+            if(lastParens(objParens) !== "[") return false;
+            else {
+                objParens["["].pop(); 
+                if(objParens["["].length === 0){
+                    delete objParens["["]
+                }
+            } 
+        }
+    }
+    if(objParens["{"]) return false;
+    if(objParens["("]) return false;
+    if(objParens["["]) return false;
 
+    return true;
 }
 
+function lastParens(obj){
+    console.log('obj:', obj)
+    
+    let lastParen = "";
+    let index =-1;
+    if(obj["{"]){
+       index= obj["{"][obj["{"].length-1];
+       lastParen = "{";
+    }
+    if(obj["("]){
+        if(obj["("][obj["("].length-1]>index){
+            index= obj["("][obj["("].length-1];
+            lastParen = "(";
+        }
+    }
+    if(obj["["]){
+        if(obj["["][obj["["].length-1]>index){
+            index= obj["["][obj["["].length-1];
+            lastParen = "[";
+        }
+    }
+      console.log('lastParen:', lastParen)
+    return lastParen;
+  
+}
+
+console.log('balancedParens([])', balancedParens('['))
 module.exports = balancedParens;
