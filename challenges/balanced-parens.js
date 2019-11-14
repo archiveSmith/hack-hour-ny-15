67d408b5d74437616a0b7cd3249e2e4406a8c145
Tim <tim@Tims-MacBook-Pro.local>
2019-11-13 21:39:11 -0500

    .

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -74,12 +74,13 @@ function balancedParens(input) {
   };
 
   for (let i = 0; i < input.length; i += 1) {
-    if (input[i] === '(' || input[i] === '{' || input[i] === '[') {
-      stackTrack.push(input[i]);
+    const currChar = input[i];
+    if (currChar === '(' || currChar === '{' || currChar === '[') {
+      stackTrack.push(currChar);
     } else {
       const lastSymbol = stackTrack.pop();
 
-      if (input[i] !== mapObj[lastSymbol]) {
+      if (currChar !== mapObj[lastSymbol]) {
         return false;
       }
     }
@@ -88,6 +89,36 @@ function balancedParens(input) {
 
   return true;
 }
+
+
+// function balancedParens(input) {
+//   //create a dictionary for possible brackets
+//   const bracketDictionary = {
+//     '[': ']',
+//     '{': '}',
+//     '(': ')'
+//   };
+//   //create a stack to store the opening brackets as a way to keep track of them
+//   const brackTrackStack = [];
+//   //iterate over inputted string
+//   for (let i = 0; i < input.length; i += 1) {
+//     //create a variable to reference the current character
+//     const currentCharacter = input[i];
+//     //ask if that character is an opening bracket
+//     //if it is, then I want to push the bracket on to my stack
+//     if (currentCharacter === '[' || currentCharacter === '{' || currentCharacter === '(') brackTrackStack.push(currentCharacter);
+//     //ask if the character is a closing bracket
+//     else if (currentCharacter === ']' || currentCharacter === '}' || currentCharacter === ')'){
+//     //create a variable to reference the last opening bracket
+//       const lastOpenedBracket = brackTrackStack.pop();
+//       //if it is, then compare it against the value returned from popping the last opening bracket from the stack and using it as a key to access the bracket dictionary
+//       //if they don't match, then return false
+//       if (bracketDictionary[lastOpenedBracket] !== currentCharacter) return false;
+//     }
+//   }
+//   //if iterator completes, return true
+//   return true;
+// }
 
 console.log(balancedParens(' var hubble = function() { telescopes.awesome();'));
 console.log(balancedParens('[(]{)}')); // false)
--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
 /* Given a positive integer, return it as a string in Roman Numeral form.
  * Some basic conversions are given below:
  *
@@ -17,7 +20,6 @@
  *
  */
 
-
 function romanNumeral(num) {
   //  create an object to hold the roman numeral list;
 
@@ -25,56 +27,46 @@ function romanNumeral(num) {
 
   // eslint-disable-next-line no-unused-vars
   const romanNumeralList = {
-    1: 'I',
-    4: 'IV',
-    5: 'V',
-    9: 'IX',
-    10: 'X',
-    40: 'XL',
-    50: 'L',
-    90: 'XC',
-    100: 'C',
-    400: 'CD',
-    500: 'D',
-    900: 'CM',
-    1000: 'M',
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
   };
 
-  let midNum;
-
-  //  if num is less than one or greater than 3999 return num
-
-  if (num < 1 || num > 3999) {
-    return num;
-  } 
-    
-    for (let key in romanNumeralList) {
-      console.log(num)
- 
-  
-      midNum = Math.floor(num / key);
-      console.log(midNum)
-  
-      console.log(midNum);
-
-      if (midNum >= 0) {
-        console.log(midNum)
-        for (let i = 0; i < midNum; i += 1) {
-          console.log(romanNumeralList[key])
-          result += romanNumeralList[key];
-        }
-      }
-
-      
-       num = num % key;
+  for (const key in romanNumeralList) {
+    console.log(num);
+    while (num >= romanNumeralList[key]) {
+      console.log(num);
+      console.log(romanNumeralList[key]);
+      // console.log()
+      result += key;
+      num -= romanNumeralList[key];
     }
-  
-  
+  }
   console.log(result);
-
   return result;
 }
 
-console.log(romanNumeral(4));
+console.log(romanNumeral(654));
 
-module.exports = romanNumeral;
+
+// //  function convertToRoman(num) {
+// //   let lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+// //   let romanized = '';
+// //   for (let i in lookup) {  	      // Loop through the key/value pairs in the lookup object
+// //     while ( num >= lookup[i] ) {  // While the condition is met do following >>>
+// //       romanized += i;   			  // Add the current i key to the variable roman
+// //       num -= lookup[i];     	  // Decrease the num number by the current i value
+// //     }
+// //   }
+// //   return romanized;
+// // };
