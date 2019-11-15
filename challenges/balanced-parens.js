3be3a088f2e00ffbb3c8fdf5007410b973230e39
Rex Osariemen <rexosariemen@gmail.com>
2019-11-15 09:56:00 -0500

    Initial Solution to hack hour problem

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,25 +24,46 @@
  *
  */
 
-function balancedParens(input) {
-  const forwardParens = '([{';
-  const backwardParens = ')]}';
-  let forwardCount = 0;
-  let backwardCount = 0;
+// function balancedParens(input) {
+//   const forwardParens = '([{';
+//   const backwardParens = ')]}';
+//   let forwardCount = 0;
+//   let backwardCount = 0;
 
+//   for (let i = 0; i < input.length; i += 1) {
+//     const char = input[i];
+//     if (forwardParens.includes(char)) {
+//       forwardCount += 1;
+//     } else if (backwardParens.includes(char)) {
+//       backwardCount += 1;
+//     }
+//   }
+//   return (forwardCount + backwardCount) % 2 === 0;
+// }
+
+const balancedParens = (input) => {
+  const parensDict = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+
+  const parensTrack = [];
   for (let i = 0; i < input.length; i += 1) {
-    const char = input[i];
-    if (forwardParens.includes(char)) {
-      forwardCount += 1;
-    } else if (backwardParens.includes(char)) {
-      backwardCount += 1;
+    const currentParen = input[i];
+    if (currentParen === '(' || currentParen === '[' || currentParen === '{') {
+      parensTrack.push(currentParen);
+    } else if (currentParen === ')' || currentParen === ']' || currentParen === '}') {
+      const prevParen = parensTrack.pop();
+      if (parensDict[prevParen] !== currentParen) return false;
     }
   }
-  return (forwardCount + backwardCount) % 2 === 0;
-}
+
+  return true;
+};
 
 // console.log(balancedParens('[](){}')); // true
-// console.log(balancedParens('[({})]'));   // true
+// console.log(balancedParens('[({})]')); // true
 // console.log(balancedParens('[(]{)}')); // false
 //   Step 3:
 // ignore non - bracket characters
--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,46 @@
  */
 
 function romanNumeral(n) {
+  const romanDictionary = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M',
+  };
+  let romanNum = '';
+  let totalTrack = 0;
+  let trunkTrack = 0;
+  for (let i = 1; i <= n; i += 1) {
+    romanNum += 'I';
+    totalTrack += 1;
+    if (totalTrack === 4) {
+      romanNum = 'IV';
+    } else if (totalTrack === 5) {
+      romanNum = 'V';
+    // } else if () {
 
+    } else {
+      // romanNum += 'I';
+    }
+  }
+
+  return romanNum;
 }
+
+console.log(romanNumeral(3));
+console.log(romanNumeral(4));
+console.log(romanNumeral(5));
+// console.log(romanNumeral(9));
+// console.log(romanNumeral(10));
+// console.log(romanNumeral(40));
 
 module.exports = romanNumeral;
