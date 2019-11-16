e84231f5b93c48c10afbfbf0687968148947392c
Ola Adedoyin <olalinux03@gmail.com>
2019-11-16 09:54:53 -0500

    completed binary to decimal

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,23 @@
  */
 
 function binToDec(binary) {
-
+  // initiate 2 to the power of n and increase the power as you move thru the string
+  // iterate thru the string and multiply each string item by 2 to the power of n starting from the right most digit
+  // add the sum of the above operation
+  // convert sum to num and return
+  let n = 0;
+  let sum = 0;
+  for (let i = binary.length - 1; i >= 0; i--) {
+    sum += (parseInt(binary[i] * Math.pow(2, n)));
+    n += 1;
+  }
+  return sum;
 }
+
+console.log(binToDec('11'));
+console.log(binToDec('100'));
+console.log(binToDec('101'));
+console.log(binToDec('0101'));
+console.log(binToDec('0'));
 
 module.exports = binToDec;
