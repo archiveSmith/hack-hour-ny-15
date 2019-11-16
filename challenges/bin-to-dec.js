eae3d661c27b75d5ca5fcb4d4cbaaad055729338
Rex Osariemen <rexosariemen@gmail.com>
2019-11-16 09:49:55 -0500

    Initial submission of hack hour

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,12 @@
  */
 
 function binToDec(binary) {
-
+  let dec = 0;
+  for (let i = 0; i < binary.length; i += 1) {
+    const num = binary[i];
+    dec += (num * 2 ** (binary.length - 1 - i));
+  }
+  return dec;
 }
 
 module.exports = binToDec;
