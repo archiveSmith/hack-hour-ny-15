f24aa63a6221bca4d4b1489691c5dffb6a90901a
Arlo Guiwo <arloguiwo@gmail.com>
2019-11-20 09:38:54 -0500

    highest prod finished

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,21 @@
  */
 
 function highestProduct(array) {
+//create permutations for subsets of 3
+    //iterate over each permutation getting the product and storing it
+    //check if current product is larger than largestProd
+if (array.length < 3) return 'not enough elements';
+array.sort((a, b) => b-a);
+return (array[0] * array[1] * array[2])
 
+
+
+//iterate over input array getting all 3-length permutations
+    //iterate over all permutations, for each element in perm array multiply all 3 elements
+        //if product is higher than current value of largestProd, reassign largestProd to current value;
 }
+
+console.log(highestProduct([1,2,3]))
 
 
 module.exports = highestProduct;
