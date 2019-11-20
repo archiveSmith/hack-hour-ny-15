81de1dacf7f2ab7747a88b0b208d76eac44f5aaf
Sejan Miah <sejan.miah@gmail.com>
2019-11-20 11:27:26 -0500

    Refactor solution

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,7 +4,22 @@
 
 function highestProduct(array) {
 
+    if (array.length < 3) {
+        return 0 
+    }
+
+    const sorted = array.sort()
+    const largestValue = sorted[sorted.length-1]
+    console.log(largestValue)
+    const secondLargest = sorted[sorted.length-2]
+    console.log(secondLargest)
+    const thirdLargest = sorted[sorted.length-3]
+    return largestValue * secondLargest * thirdLargest
+
 }
+
+
+console.log(highestProduct([5, 1]))
 
 
 module.exports = highestProduct;
