b8d98f92f3b0eced03f0187ccc1dc556edb00fc7
anna <anna.svetlov@gmail.com>
2019-11-20 09:48:31 -0500

    Now handling negative numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -5,6 +5,10 @@
 function highestProduct(array) {
     if (Array.isArray(array) && array.length > 3) {
         const sortedArray = array.sort((a,b) => a-b)
+        if (sortedArray[0] < 0 && sortedArray[1] < 0 && Math.abs(sortedArray[0]) > sortedArray[sortedArray.length-3] && Math.abs(sortedArray[1]) > sortedArray[sortedArray.length-3]) {
+            sortedArray.push(Math.abs(sortedArray[0]))
+            sortedArray.push(Math.abs(sortedArray[1]))
+        }
         // console.log(sortedArray)
         return (sortedArray[sortedArray.length-1] * sortedArray[sortedArray.length-2] * sortedArray[sortedArray.length-3])
     }
@@ -16,4 +20,4 @@ function highestProduct(array) {
 
 module.exports = highestProduct;
 
-// console.log(highestProduct([-2,-5,-3,-25,-6,-9]))
+// console.log(highestProduct([2, 5, 3, -25, -6, 9]))
