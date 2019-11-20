8f60b263fd9b3e79be101a4d242a78ab8fe68fdf
bahrambds <bahrambds@gmail.com>
2019-11-20 09:40:41 -0500

    completed highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
-
+    let doubleArray = array.concat(array)
+    // console.log(doubleArray)
+    return Math.max(...doubleArray.reduce((acc, next, index, array) => {
+        acc.push((array[index]*array[index+1]*array[index+2]).toString())
+        return acc
+    },[]).filter(e => e !== 'NaN'))
 }
+
+console.log(highestProduct([2,4,5,6]))
 
 
 module.exports = highestProduct;
