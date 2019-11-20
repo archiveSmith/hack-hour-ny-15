08ac018a91e46a301be501777371df0abe13dc62
Ola Adedoyin <olalinux03@gmail.com>
2019-11-20 09:58:19 -0500

    completed

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,22 @@
  */
 
 function highestProduct(array) {
-
+  let highestProd = 1;
+  if (array.length < 3) return -1;
+  const sortedArray = array.sort((a, b) => a - b);
+  //console.log(sortedArray);
+  let i = 0;
+  while (i < 3) {
+    const lastEl = sortedArray.pop();
+    //console.log(lastEl);
+    highestProd *= lastEl;
+    //console.log(highestProd);
+    i += 1;
+  }
+  return highestProd;
 }
 
+
+//console.log(highestProduct([4, 2, 5, 1, 3]));
 
 module.exports = highestProduct;
