e8a1c87612783dd7ff5b7ad95f8a226357f7b92a
anna <anna.svetlov@gmail.com>
2019-11-20 09:27:46 -0500

    Multiplying largest three nums?

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
-
+    if (array.length < 3) {
+        return 'Minimum array of 3 required'
+    }
+    const sortedArray = array.sort((a,b) => a-b)
+    console.log(sortedArray)
+    return (sortedArray[sortedArray.length-1] * sortedArray[sortedArray.length-2] * sortedArray[sortedArray.length-3])
 }
 
 
 module.exports = highestProduct;
+
+// console.log(highestProduct([2,5,3,25,6,9]))
