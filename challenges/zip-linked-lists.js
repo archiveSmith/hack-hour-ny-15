65a0a76632430e05a4f198f60f0c3f56a0172067
James Gary <gmarchesi888@gmail.com>
2019-11-22 10:00:57 -0500

    Didn't get very far.

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,43 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
+var list1 = {
+  data: 1,
+  next: {
+    data: 3,
+    next: {
+      data: 5,
+      next: null
+    }
+  }
 };
 
-module.exports = {Node: Node, zip: zip};
+var list2 = {
+  data: 2,
+  next: {
+    data: 4,
+    next: {
+      data: 6,
+      next: null
+    }
+  }
+};
+
+// set an empty object as the target of the data
+// Iterate, alternating through both lists:
+//    set 'data' on current node of target object equal to 'data' of current node of list A;
+//    set 'next' on current node of target object to 'data' of list B;
+
+function zip(l1, l2) {
+  const listOut = {};
+  let l1Head = l1.next;
+  let l2Head = l2.next;
+
+  while (l1Head !== null && l2Head !== null) {
+    listOut.data = currentNode.data;
+
+    l1Head = currentNode.next;
+  }
+}
+
+module.exports = { Node: Node, zip: zip };
