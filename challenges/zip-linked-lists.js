adc304a38f16d2d4a87488b79e0773fac5530c04
Chris Romano <chrispaulromano@gmail.com>
2019-11-23 09:23:13 -0500

    precautionary

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,23 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  let sever = l1.next;
+  l1.next = l2;
+  sever = l2.next;
+  console.log(l1);
+}
 
-module.exports = {Node: Node, zip: zip};
+
+const l1 = new Node(1);
+l1.next = new Node(2);
+l1.next.next = new Node(3);
+console.log(l1);
+const l2 = new Node('a');
+l2.next = new Node('b');
+l2.next.next = new Node('c');
+console.log(l2);
+
+zip(l1, l2);
+
+
+module.exports = { Node, zip };
