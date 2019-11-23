a8a5dcd7f65d7ce3d32079eff35df5b800f5bf55
jenae <jenaepen@gmail.com>
2019-11-23 10:10:53 -0500

    o1 lookup

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -12,17 +12,57 @@ function Node(val) {
 
 function zip(l1, l2) {
 
-  let head = l1;
-
-  function zipR(l1,l2,currentNode){
-    if(l2) currentNode.next= l2 
-    if(l1) currentNode.next.next= l1
-    if(!l1 && !l2) return;
-    return zip(l1.next,l2.next, currentNode.next.next) 
-  }
+  let head = new Node(l1.value);
+  
   zipR(l1.next,l2, head);
+  
+  function zipR(l1,l2, currentNode){ 
+    if(l1 === null && l2 === null) return;
+    if(l2) {
+      currentNode.next= new Node (l2.value); 
+      return zipR(l2.next,l1, currentNode.next) 
+    }
+    else {
+      return zipR(l2,l1, currentNode)
+    }
+  }  
+  
+ 
   return head;
 };
+let node1 = new Node("1");
+let node2 = new Node("2");
+let node3 = new Node("3");
+let node4 = new Node("4");
+ node1.next= node2;
+ node2.next= node3;
+ node3.next= node4;
+
+
+
+
+
+
+
+ 
+
+
+let nodea = new Node("a");
+let nodeb = new Node("b");
+let nodec = new Node("c");
+let noded = new Node("d");
+
+nodea.next= nodeb;
+nodeb.next= nodec;
+nodec.next= noded;
+
+let head = zip(node1,nodea)
+let node = head;
+while(node) {
+  console.log('final node:', node)
+  node = node.next;
+}
+
 
 
 module.exports = {Node: Node, zip: zip};
