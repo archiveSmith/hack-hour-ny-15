69354c7f0abbd144fa5e70325634fce61fb89bdc
Ola Adedoyin <olalinux03@gmail.com>
2019-11-23 01:10:40 -0500

    not tested

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,45 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  if (!l2) return l1;
 
-module.exports = {Node: Node, zip: zip};
+
+  let currPointer = l1.head;
+  let nextPointer = currPointer.next;
+  let pointer2 = l2.head.next;
+
+  while (pointer2 !== null) {
+    currPointer.next = l2.head;
+    l2.head.next = nextPointer;
+
+    currPointer = nextPointer;
+    nextPointer = nextPointer.next;
+
+    l2.head = pointer2;
+    pointer2 = pointer2.next;
+
+
+    if (pointer2 === null) {
+      currPointer.next = l2.head;
+    }
+  }
+  return l1;
+}
+
+
+// const l1 = new Node(1);
+// l1.next = new Node(2);
+// l1.next.next = new Node(3);
+
+// const l2 = new Node(4);
+// l2.next = new Node(5);
+// l2.next.next = new Node(6);
+
+
+// console.log(l1);
+// console.log(l2);
+
+// console.log(zip(l1, l2));
+
+
+module.exports = { Node, zip };
