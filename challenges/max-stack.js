127ae451a8ddab322a817742595ad02b4cb96161
Sejan Miah <sejan.miah@gmail.com>
2019-11-23 09:48:57 -0500

    Can't solve for O(1) O(well)

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,56 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.index = 0
 }
+
+//GO OVER REST OPERATOR IN THIS SITUATION
+Stack.prototype.push = function(...value) {
+  for (let element of value) {
+    this.storage[this.index++]= element
+  }
+
+  return this.index
+}
+
+Stack.prototype.pop = function() {
+  if (this.index === 0) {
+    return undefined
+  } else {
+    //get top most element from the stack
+    const removed = this.storage[this.index-1];
+    delete this.storage[--this.index-1];
+    return removed
+  }
+}
+
+Stack.prototype.getMax = function() {
+
+  //get all the key value pairs in the storage
+  let max = this.storage[0]
+
+  
+  for(let value of Object.values(this.storage)) {
+
+    if (max < value) {
+      max = value
+    }
+
+    if (typeof max === 'string') {
+      max = Number(max)
+    }
+  }
+  console.log(max)
+  return max
+}
+
+let newStack = new Stack
+
+newStack.push(10);
+newStack.push(20);
+
+console.log(newStack.getMax())
+// newStack.pop()
 
 module.exports = Stack;
--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,23 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  //L1 is the first zipped list
+  //L2 is the second zipped list
+
+  //Create an array and all the even number indices you set equal to the first zipped linked list
+  //In the same array set all the odd number value indices as the values for the second zipped linked list
+
+  //then you begin actually setting the node values in a separate faux linked list class
+
+const firstNode = new Node(1)
+const secondNode = new Node(2)
+firstNode.next = secondNode
+
+  console.log(firstNode)
 };
+
+zip()
+
+
 
 module.exports = {Node: Node, zip: zip};
