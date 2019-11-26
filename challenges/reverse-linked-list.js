6a84dce07cbd30ec25718cd771bfe43091c1652a
Cedric Theofanous <cedrictheo@gmail.com>
2019-11-26 09:44:19 -0500

    complete reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,27 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+  // start at head and iterate, storing each node value in a stack
+  // declare last node as new head to return and then iterate to create new nodes with values in storage
+  const nodeStorage = [];
+  let nodeCount = 0;
+  while (head.next !== null) {
+    nodeStorage.push(head.value);
+    nodeCount += 1;
+    head = head.next;
+  }
+  const headToReturn = head;
+  while (nodeCount > 0) {
+    head.next = new Node(nodeStorage.pop());
+    nodeCount -= 1;
+    head = head.next;
+  }
+  return headToReturn;
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+module.exports = { Node, reverseLinkedList };
