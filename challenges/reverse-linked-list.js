7c272c9f614a2361b80c8c1686cf1a345789e9e7
jenae <jenaepen@gmail.com>
2019-11-26 10:00:36 -0500

    hi

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,33 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    const array = [];
+    let node = head;
+    while(node){
+        array.push(node);
+        node= node.next;
+    }
+    for(let i=array.length-1; i>0; i--){
+        array[i].next = array[i-1]
+    }
+    array[0].next=null;
+    console.log('array:', array)
+    return array[array.length-1];
 }
+
+
+let nodea = new Node(5);
+let nodeb = new Node(4);
+let nodec = new Node(3);
+let noded = new Node(2);
+let nodee = new Node(1);
+
+nodea.next= nodeb;
+nodeb.next= nodec;
+nodec.next= noded;
+noded.next= nodee;
+
+console.log('reverseLinkedList(nodea):', reverseLinkedList(nodea))
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
