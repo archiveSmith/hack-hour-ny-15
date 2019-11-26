c402accc978530f929676fb8bc6f4d70749521c8
Rex Osariemen <rexosariemen@gmail.com>
2019-11-26 09:57:05 -0500

    HackHour Attempt

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,42 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
+
+// function reverseLinkedList(head) {
+//   if (!head.next) return head;
+//   let curNode = head.next;
+//   const headCopy = head;
+//   while (head.next) {
+//     // let penultimateNode =
+//     if (!curNode.next) {
+//       head = curNode;
+//       headCopy.next = null;
+//       return head;
+//     }
+//     curNode = curNode.next;
+//   }
+// }
 
 function reverseLinkedList(head) {
+  // return head if there are no other node in the linkList
+  let node = head;
+  // initialize tracking variable to be reasigned as I move from left to right;
+  let previous;
+  let temp;
+  // Loop through from left to right,
+  while (node) {
+    temp = node.next;
+    node.next = previous;
 
+    // when temp becomes null, then the while loop is exited;
+    previous = node;
+    node = temp;
+  }
+
+  return previous;
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+module.exports = { Node, reverseLinkedList };
