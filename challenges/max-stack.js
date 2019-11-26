801c6f387c835a3e243a34dbb256aa0364884137
Rex Osariemen <rexosariemen@gmail.com>
2019-11-26 09:22:00 -0500

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,60 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
+// function Stack() {
+//   // body...
+// }
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(val) {
+    if (this.items.length === 0) {
+      this.items[0] = val;
+    } else {
+      this.items[this.items.length] = val;
+    }
+    return this.items.length;
+  }
+
+  pop() {
+    const poppedElement = this.items[this.items.length - 1];
+    this.items.length = this.items.length - 1;
+    return poppedElement;
+  }
+
+  getMax() {
+    let max = null;
+    let curMax;
+
+    for (let i = 0; i < this.items.length; i += 1) {
+      const frontElement = this.items[i];
+      const backElement = this.items[this.items.length - 1 - i];
+      curMax = frontElement;
+      if (frontElement >= backElement) {
+        curMax = frontElement;
+        if (curMax > max) {
+          max = curMax;
+          curMax = null;
+        }
+      } else if (backElement > frontElement) {
+        curMax = backElement;
+        if (curMax > max) {
+          max = curMax;
+          curMax = null;
+        }
+      } else {
+        curMax = backElement;
+        if (curMax > max) {
+          max = curMax;
+          curMax = null;
+        }
+      }
+    }
+    return max;
+  }
 }
 
 module.exports = Stack;
