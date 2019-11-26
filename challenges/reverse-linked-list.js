e9aacfe96dd91af5b08e7761fce0234c811c4c86
Joseph Michael Corrado <joeyycorrss@gmail.com>
2019-11-26 09:33:18 -0500

    easierInPlace

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,8 +13,35 @@ function Node(value) {
     this.next = null;
 }
 
-function reverseLinkedList(head) {
+function reverseLinkedList( head ) {
+    // INITIALIZE NEWHEAD AS NEW NODE W/ HEAD VALUE TO START FROM 
+    let newHead = new Node( head.value );
+    // INTIALIZE KEY VALUES FOR WHILE LOOP
+    let pointer = head;
 
+    // ITERATE THROUGH OG LINKED LIST TILL YOU HIT NULL
+    // USE POINTER.NEXT TO MAKE NEW NODE AND SET ITS
+    // NEXT TO NEWHEAD, FINALLY CHANGE POINTER TO POINTER.NEXT
+    while ( pointer.next !== null ) {
+      let nextNode = new Node( pointer.next.value );
+      nextNode.next = newHead;
+      newHead = nextNode;
+      pointer = pointer.next;
+    }
+    // RETURN NEWHEAD
+    return newHead;
 }
+
+// let one = new Node( 1 );
+// let two = new Node( 2 );
+// let three = new Node( 3 );
+// let four = new Node( 4 );
+// let five = new Node( 5 );
+// one.next = two;
+// two.next = three;
+// three.next = four;
+// four.next = five;
+
+// console.log(reverseLinkedList(one)); 
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
