52d4a5810e067d4e24b26f1ae9494d592bfd1b32
James Gary <gmarchesi888@gmail.com>
2019-12-03 10:42:21 -0500

    Done...the Scratch Project is important, so I relied heavily on Google and StackOverflow to solve the HH problem.

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -39,10 +39,45 @@ Stack.prototype.size = function() {
  * Queue Class
  */
 
-function Queue() {}
+function Queue() {
+  this.storage = {};
+  this.count = 0;
+  this.lowestCount = 0;
+}
+
+// Adds a value to the end of the chain
+Queue.prototype.enqueue = function(value) {
+  // Check to see if value is defined
+  if (value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+};
+
+// Removes a value from the beginning of the chain
+Queue.prototype.dequeue = function() {
+  // Check to see if queue is empty
+  if (this.count - this.lowestCount === 0) {
+    return undefined;
+  }
+
+  var result = this.storage[this.lowestCount];
+  delete this.storage[this.lowestCount];
+  this.lowestCount++;
+  return result;
+};
+
+// Returns the length of the queue
+Queue.prototype.size = function() {
+  return this.count - this.lowestCount;
+};
 
 const myStack = new Stack();
 myStack.push(-999);
 console.log(myStack);
+const myQueue = new Queue();
+myQueue.enqueue(myStack);
+myQueue.enqueue(myStack);
+console.log(myQueue);
 
 module.exports = { Stack: Stack, Queue: Queue };
