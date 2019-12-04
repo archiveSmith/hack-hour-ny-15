e013664ffe94503a4da32bf547b3e26c5c4ee24a
Rex Osariemen <rexosariemen@gmail.com>
2019-12-04 09:47:03 -0500

    Two sum solution

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -12,12 +12,34 @@
  *                  sampleGrid after:   [   [7, 4, 1],
  *                                          [8, 5, 2],
  *                                          [9, 6, 3]  ]
- *
+ *[[1, 2, 3], [4, 5, 6], [7, 8, 9]] => [[7, 4, 1], [8, 5, 2], [9, 6, 3]];
  * BONUS: Do this in place
  */
 
 function rotateGrid(grid, n) {
+  const newArr = [];
+  let j = 0;
+  let inner = [];
 
+  for (let i = 0; i < grid.length; i += 1) {
+    const subArr = grid[i];
+    for (let j = 0; j < n; j += 1) {
+    newArr.unshift(grid[i][j]);
+    }
+    // for (let j = 0; j < n; j += 1) {
+    //   inner.unshift(grid[i][j]);
+    // }
+    console.log(inner);
+    if (inner.length === n) {
+    newArr.push(inner);
+    inner = [];
+    j += 1;
+    };
+  }
+  console.log(newArr);
+  return newArr;
 }
+
+console.log(rotateGrid([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3));
 
 module.exports = rotateGrid;
--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,33 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+// function twoSum(arr, n) {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     for (let j = i + 1; i < arr.length; i += 1) {
+//       if (arr[i] + arr[j] === n) return true;
+//     }
+//   }
+//   return false;
+// }
 
+function twoSum(arr, n) {
+  const array = arr.sort();
+  for (let i = 0; i < array.length; i += 1) {
+    let frontIndex = i;
+    let backIndex = array.length - i - 1;
+    if (array[frontIndex] + array[backIndex] > n) {
+      backIndex -= 1;
+    } else if (array[frontIndex] + array[backIndex] < n) {
+      frontIndex += 1
+    } else if (array[frontIndex] + array[backIndex] === n) {
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log(twoSum([1, 3, 5, 6], 8));
+console.log(twoSum([1, 3, 5, 6], 45));
+// console.log([3, 1, 6, 8].sort())
 
 module.exports = twoSum;
