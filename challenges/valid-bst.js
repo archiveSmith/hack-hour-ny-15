50841b5b927649164ec50a28bcdeae747fca5403
James Gary <gmarchesi888@gmail.com>
2019-12-05 09:39:35 -0500

    Done, sort of...

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,16 +4,79 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
 
+// Copied from https://js-algorithms.tutorialhorizon.com/2015/12/13/determine-if-a-binary-tree-is-a-binary-search-tree/
+function BinaryTree() {
+  this.root = null;
+}
+
+var last_logged;
+
+function isBST(root) {
+  if (root === null) {
+    // base case
+    return true;
+  }
+
+  // Verify and recurse left
+  if (!isBST(root.left)) {
+    return false;
+  }
+
+  // Verify the current node
+  if (last_logged !== null && root.data <= last_logged) {
+    return false;
+  }
+
+  // Log the current data for debugging and update the last_logged
+  console.log("Current Node : ", root.data);
+  last_logged = root.data;
+
+  // Verify and recurse left
+  if (!isBST(root.right)) {
+    return false;
+  }
+
+  return true;
+}
+
+// Create a Binary Tree as a sample input
+var root = {
+  data: 8,
+  left: null,
+  right: null
+};
+var n1 = {
+  data: 10,
+  left: null,
+  right: null
+};
+
+var n2 = {
+  data: 6,
+  left: null,
+  right: null
+};
+
+var BT = new BinaryTree();
+BT.root = root;
+
+// Create a Binary Search Tree (BST) and Verify
+BT.root.left = n2;
+BT.root.right = n1;
+console.log(isBST(BT.root)); // true
+
+// Create a non-BST and Verify
+BT.root.left = n1;
+console.log(isBST(BT.root)); // false
+
+// original supplied material:
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
-function validBST(tree) {
+function validBST(tree) {}
 
-}
-
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
