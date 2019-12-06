44d3e0d343da5bb0352a77c8218d38aa5f4f19cd
jenae <jenaepen@gmail.com>
2019-12-06 09:38:59 -0500

    finished gcd.js

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,16 @@
  */
 
 function gcd(a, b) {
+    let divisor = 1;
 
+    for(let i = 2; i<= Math.min(a,b); i++){
+        if(a % i === 0 && b % i === 0) divisor = i;    
+    } 
+    return divisor;
 }
-
+ 
+ console.log(' gcd(100, 120):',  gcd(100, 120))
+ console.log(' gcd(30, 100):',  gcd(30, 100))
+ console.log(' gcd(1000, 100):',  gcd(1000, 100))
+ console.log(' gcd(100, 100):',  gcd(100, 100))
 module.exports = gcd;
