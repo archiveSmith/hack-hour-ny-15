16b6e7604a27d146a1e2c570a5e17cb6ac97e404
Rex Osariemen <rexosariemen@gmail.com>
2019-12-06 13:19:18 -0500

    Commit syntax change on bst

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,15 +8,19 @@
  */
 
 function gcd(a, b) {
+  // initialize the greatest gcd to null to account for -ive nums
   let greatest = null;
+  // starting from 1, check for numbers that can divide both a & b until its either a or b;
   for (let i = 1; i <= a; i += 1) {
     let curGreatest = null;
+    // if the current i is greater than the greatest divides both a & b, and want to assign it to curGreatest;
     if ((a % i === 0 && b % i === 0) && curGreatest < i) {
       curGreatest = i;
+      // only reassign curGreatest to greatest if greatest < curGreatest;
       if (greatest < curGreatest) greatest = curGreatest;
     }
   }
-
+  // return the greatest which by now will be curGreatest;
   return greatest;
 }
 
