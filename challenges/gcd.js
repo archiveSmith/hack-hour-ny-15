df5651f40d2e1668cf10d908df7525c7282a2d11
James Gary <gmarchesi888@gmail.com>
2019-12-06 09:43:17 -0500

    Done with GCD.

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,21 @@
  */
 
 function gcd(a, b) {
-
+  const workArr = [a, b].sort((a, b) => b - a);
+  let bigNum = workArr[0];
+  let littleNum = workArr[1];
+  let remainder = bigNum % littleNum;
+  while (remainder !== 0) {
+    console.log(bigNum, littleNum, remainder);
+    bigNum = littleNum;
+    littleNum = remainder;
+    remainder = bigNum % littleNum;
+  }
+  return littleNum;
 }
+
+console.log(gcd(10, 8));
+console.log(gcd(100, 25));
+console.log(gcd(28, 21));
 
 module.exports = gcd;
